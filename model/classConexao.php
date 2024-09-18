d902b7066a4fab5514ea88687290655ca4844a48
yiguinn <igorprimo.br@gmail.com>
2024-09-18 16:30:37 -0300

    att

--- model/classConexao.php
+++ model/classConexao.php
@@ -34,15 +34,14 @@
             $insere->bindValue(":ufEnd",$ufEndC);
         }
 
-        public function insereEstoque($cepEndC, $ruaEndC, $numeroEndC, $bairroEndC, $cidadeEndC, $ufEndC) {
-            $insere = $this->pdo->prepare("insert into endereco(cepEndereco, numeroEndereco, tipoEndereco, ruaEndereco, cidadeEndereco, bairroEndereco, ufEndereco)
-            values (:cepEnd, :numeroEnd, :tipoEnd, :ruaEnd, :cidadeEnd, :bairroEnd, :ufEnd)");
-            $insere->bindValue(":cepEnd",$cepEndC);
-            $insere->bindValue(":numeroEnd",$numeroEndC);
-            $insere->bindValue(":ruaEnd",$ruaEndC);
-            $insere->bindValue(":bairroEnd",$bairroEndC);
-            $insere->bindValue(":cidadeEnd",$cidadeEndC);
-            $insere->bindValue(":ufEnd",$ufEndC);
+        public function insereEstoque($loteEstqC, $dataEstqC, $quantidadeEstqC, $precoCustoC, $precoVendaC) {
+            $insere = $this->pdo->prepare("insert into estoque(loteEstoque, dataEstoque, qtdEstoque, precoCusto, precoVenda)
+            values (:loteEstq, :dataEstq, :qtdEstq, :precoCustoEstq, :precoVendaEstq)");
+            $insere->bindValue(":loteEstq",$loteEstqC);
+            $insere->bindValue(":dataEstq",$dataEstqC);
+            $insere->bindValue(":qtdEstq",$quantidadeEstqC);
+            $insere->bindValue(":precoCustoEstq",$precoCustoC);
+            $insere->bindValue(":precoVendaEstq",$precoVendaC);
         }
         
     }
