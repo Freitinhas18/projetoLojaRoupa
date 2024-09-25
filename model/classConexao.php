16a0f8ebe2a303dafd936b0afce41a6b0ba59d26
dudsbtw <eduardo@gmail.com>
2024-09-25 08:12:14 -0300

    att

--- model/classConexao.php
+++ model/classConexao.php
@@ -22,29 +22,28 @@
             }
         }
 
-        public function insereCliente($nomeClienteC, $cpfClienteC, $telClienteC, $emailClienteC){
-            $insere = $this->pdo->prepare("insert into cliente(nomeCliente, cpfCliente, telCliente, emailCliente)
-            values (:nomeCliente, :cpfCliente, :telCliente, :emailCliente)");
+        public function insereCliente($nomeClienteC, $cpfClienteC, $telClienteC, $emailClienteC, $numeroEndC, $complResidClienteC, $obsResidClienteC){
+            $insere = $this->pdo->prepare("insert into cliente(nomeCliente, cpfCliente, telCliente, emailCliente, numeroEndereco, complResidCliente, obsResidCliente)
+            values (:nomeCliente, :cpfCliente, :numeroEnd, :telCliente, :emailCliente, :complResidCliente, :obsResidCliente)");
             $insere->bindValue(":nomeCliente",$nomeClienteC);
             $insere->bindValue(":cpfCliente",$cpfClienteC);
             $insere->bindValue(":telCliente",$telClienteC);
             $insere->bindValue(":emailCliente",$emailClienteC);
-            $insere->execute();
-        }
-
-        public function insereEndereco($cepEndC, $ruaEndC, $numeroEndC, $bairroEndC, $cidadeEndC, $ufEndC, $complResidClienteC, $obsResidClienteC) {
-            $insere = $this->pdo->prepare("insert into endereco(cepEndereco, numeroEndereco, ruaEndereco, cidadeEndereco, bairroEndereco, ufEndereco, complResidCliente, obsResidCliente)
-            values (:cepEnd, :numeroEnd, :ruaEnd, :cidadeEnd, :bairroEnd, :ufEnd, :complResidCliente, :obsResidCliente)");
-            $insere->bindValue(":cepEnd",$cepEndC);
-            $insere->bindValue(":ruaEnd",$ruaEndC);
             $insere->bindValue(":numeroEnd",$numeroEndC);
-            $insere->bindValue(":bairroEnd",$bairroEndC);
-            $insere->bindValue(":cidadeEnd",$cidadeEndC);
-            $insere->bindValue(":ufEnd",$ufEndC);
             $insere->bindValue(":complResidCliente",$complResidClienteC);
             $insere->bindValue(":obsResidCliente",$obsResidClienteC);
             $insere->execute();
-            
+        }
+
+        public function insereEndereco($cepEndC, $ruaEndC, $bairroEndC, $cidadeEndC, $ufEndC) {
+            $insere = $this->pdo->prepare("insert into endereco(cepEndereco,  ruaEndereco, cidadeEndereco, bairroEndereco, ufEndereco)
+            values (:cepEnd, :ruaEnd, :cidadeEnd, :bairroEnd, :ufEnd)");
+            $insere->bindValue(":cepEnd",$cepEndC);
+            $insere->bindValue(":ruaEnd",$ruaEndC);
+            $insere->bindValue(":bairroEnd",$bairroEndC);
+            $insere->bindValue(":cidadeEnd",$cidadeEndC);
+            $insere->bindValue(":ufEnd",$ufEndC);
+            $insere->execute();
         }
 
         public function insereEstoque($loteEstqC, $dataEstqC, $quantidadeEstqC, $precoCustoC, $precoVendaC) {
