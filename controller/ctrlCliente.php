31577e4fd30af996459714e4666c3ea09e6c7c4e
dudsbtw <eduardo@gmail.com>
2024-09-25 08:12:17 -0300

    att

--- controller/ctrlCliente.php
+++ controller/ctrlCliente.php
@@ -6,9 +6,12 @@
     $cpfClienteC = addslashes($_POST['cpfCliente']);
     $telClienteC = addslashes($_POST['telCliente']);
     $emailClienteC = addslashes($_POST['emailCliente']);
+    $numeroEndC = addslashes($_POST['numeroEnd']);
+    $complResidClienteC = addslashes($_POST['complResidCliente']);
+    $obsResidClienteC = addslashes($_POST['obsResidCliente']);
 
     $clienteC = new conexao();
-    $clienteC->insereCliente($nomeClienteC, $cpfClienteC, $telClienteC, $emailClienteC);
+    $clienteC->insereCliente($nomeClienteC, $cpfClienteC, $telClienteC, $emailClienteC, $numeroEndC, $complResidClienteC, $obsResidClienteC);
 
     echo"<br><br>";
     echo"<div> class='tableCliente'<h2>CLIENTE CADASTRADO</h2>.";
--- controller/ctrlEndereco.php
+++ controller/ctrlEndereco.php
@@ -4,15 +4,13 @@
     
     $cepEndC = addslashes($_POST['cepEnd']);
     $ruaEndC = addslashes($_POST['ruaEnd']);
-    $numeroEndC = addslashes($_POST['numeroEnd']);
     $bairroEndC = addslashes($_POST['bairroEnd']);
     $cidadeEndC = addslashes($_POST['cidadeEnd']);
     $ufEndC = addslashes($_POST['ufEnd']);
-    $complResidClienteC = addslashes($_POST['complResidCliente']);
-    $obsResidClienteC = addslashes($_POST['obsResidCliente']);
+    
 
     $enderecoC = new conexao();
-    $enderecoC->insereEndereco($cepEndC, $ruaEndC, $numeroEndC, $bairroEndC, $cidadeEndC, $ufEndC, $complResidClienteC, $obsResidClienteC);
+    $enderecoC->insereEndereco($cepEndC, $ruaEndC, $bairroEndC, $cidadeEndC, $ufEndC);
 
     echo"<br><br>";
     echo"<div class='tableEndereco'> <h2>ENDEREÇO CADASTRADO</h2>.";
